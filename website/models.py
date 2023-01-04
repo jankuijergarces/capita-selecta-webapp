e89a8918f0337a80d36ffd9874d0a2faaadb6ede
jankuijergarces <75396957+jankuijergarces@users.noreply.github.com>
2023-01-04 14:47:05 +0100

    New pipeline edit

--- website/models.py
+++ website/models.py
@@ -2,6 +2,7 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
+
 class Note(db.Model):
     __tablename__ = 'note'
     id = db.Column(db.Integer, primary_key=True)
