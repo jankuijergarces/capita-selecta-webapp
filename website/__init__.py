6e0becf0fdc7e9f8f63f6bc8be5050578104c8f7
jankuijergarces <75396957+jankuijergarces@users.noreply.github.com>
2023-01-11 13:57:06 +0100

    Changes to Azure Pipeline

--- website/__init__.py
+++ website/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_migrate import Migrate
+from flask_migrate import Migrate 
 
 DB_USER = 'fyrzfivvbn'
 DB_PASS = '16S1A2Q64OGQF8A7$'
@@ -9,31 +9,30 @@ DB_HOST = 'capita-selecta-webapp-db-server.postgres.database.azure.com'
 DB_NAME = 'postgres'
 
 db = SQLAlchemy()
-migrate = Migrate(db)  
+migrate = Migrate(db)
+  
+app = Flask(__name__)
 
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'secretkey123'
-    app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql+psycopg2://{DB_USER}:{DB_PASS}$@{DB_HOST}/{DB_NAME}"
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    db.init_app(app)
+app.config['SECRET_KEY'] = 'secretkey123'
+app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql+psycopg2://{DB_USER}:{DB_PASS}$@{DB_HOST}/{DB_NAME}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
 
-    from .views import views
-    from .auth import auth
+from .views import views
+from .auth import auth
 
-    app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
+app.register_blueprint(views, url_prefix='/')
+app.register_blueprint(auth, url_prefix='/')
 
-    from .models import User, Note
+from .models import User, Note
 
-    login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
-    login_manager.init_app(app)
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
 
-    @login_manager.user_loader
-    def load_user(id):
-        return User.query.get(int(id))
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
-    return app
 
 
